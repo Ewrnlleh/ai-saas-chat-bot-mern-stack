60d24793b50a82cc919478d70ebee4cd9de470df
Ewrnlleh <ewrnlleh@hotmail.com>
2025-09-16 21:16:44 +0300

    Add Express router setup for user routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { config } from 'dotenv';
+import morgan from 'morgan';
+import appRouter from './routes/index.js';
 
 // Load environment variables from .env file
 config();
@@ -8,5 +10,10 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+
+//remove it in production
+app.use(morgan('dev'));
+
+app.use("/api/v1", appRouter);
 
 export default app;
