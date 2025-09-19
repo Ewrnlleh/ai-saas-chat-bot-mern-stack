0df5b84aa55abb72d3506ba6d3f29c7a64d67e67
Ewrnlleh <ewrnlleh@hotmail.com>
2025-09-19 21:23:47 +0300

    Define chats field in user schema to use chatSchema for structured data

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -11,7 +11,9 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    chats: [],
+    chats: [chatSchema],
 
 });
+
+export default mongoose.model("User", userSchema);
 
